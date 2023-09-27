856b9a5bc1c1b0497b32f202413b0cdcc64cb31f
AlexeyNerobeev <alex_nerob@mail.ru>
2023-09-27 16:27:30 +0500

    добавлена 6 практика

---
+++ PZ_06/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = new int[10];
+
+        Random random = new Random();
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(0, 51);
+        }
+
+        Array.Sort(array, 0, array.Length / 2);
+        Array.Sort(array, array.Length / 2, array.Length / 2, new ReverseComparer());
+
+        Console.WriteLine("Отсортированный массив:");
+        foreach (int num in array)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
+
+class ReverseComparer : IComparer<int>
+{
+    public int Compare(int x, int y)
+    {
+        return y.CompareTo(x);
+    }
+}
