751d30d2449fcc0d3e26e5ea19eb19b5aea74b50
AlexeyNerobeev <alex_nerob@mail.ru>
2023-11-21 18:56:33 +0500

    добавлена 5 практика

---
+++ PZ_05/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Таблица значений функции y = f(x):");
+            Console.WriteLine("x\t|\ty");
+            int x = -2;
+            double t = 3.14; // Произвольное значение для t
+            while (x <= 12)
+            {
+                double y;
+                if (x < 0)
+                {
+                    y = t;
+                }
+                else if (x < 10)
+                {
+                    y = t * x;
+                }
+                else
+                {
+                    y = 2 * t;
+                }
+                Console.WriteLine($"{x}\t|\t{y}");
+                x += 2;
+            }
+            Console.WriteLine();
+            x = -2;
+            do
+            {
+                double y;
+                if (x < 0)
+                {
+                    y = t;
+                }
+                else if (x < 10)
+                {
+                    y = t * x;
+                }
+                else
+                {
+                    y = 2 * t;
+                }
+                Console.WriteLine($"{x}\t|\t{y}");
+                x += 2;
+            } while (x <= 12);
+        }
+    }
+}
