dbc1ba5e232f2e4d5926f63a1b5baee793975737
AlexeyNerobeev <alex_nerob@mail.ru>
2023-11-28 18:40:31 +0500

    добавлена 12 практика

---
+++ PZ_12/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace PZ_12
+{
+    internal class Program
+    {
+            static int[] CalculateDigitSums(int[] nums)
+            {
+                int[] results = new int[nums.Length];
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    int sum = 0;
+                    foreach (char c in nums[i].ToString())
+                    {
+                        int digit = c - '0';
+                        sum += digit;
+                    }
+
+                    results[i] = sum;
+                }
+
+                return results;
+            }
+
+            static void Main()
+            {
+                int[] nums = { 458, 657, 547, 215, 546 };
+                int[] results = CalculateDigitSums(nums);
+
+                for (int i = 0; i < results.Length; i++)
+                {
+                    Console.WriteLine(results[i]);
+                }
+            }
+    }
+}
